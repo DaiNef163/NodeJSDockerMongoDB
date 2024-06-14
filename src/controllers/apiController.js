e53b0cc144c73f9e43eeca80c40722613748f599
hoainam <hoainambld2003@gmail.com>
2024-06-14 23:33:41 +0700

    restful api

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -11,20 +11,53 @@ const getUsersAPI = async (req, res) => {
 const postCreateUserAPI = async (req, res) => {
   let { email, name, city } = req.body;
 
-    let user = await User.create({
-      name,
-      email,
-      city,
-    });
+  let user = await User.create({
+    name,
+    email,
+    city,
+  });
 
-    // console.log(results);
-    // res.redirect("/display");
-    return res.status(200).json({
-      EC: 0,
-      data: user,
-    });
+  // console.log(results);
+  // res.redirect("/display");
+  return res.status(200).json({
+    EC: 0,
+    data: user,
+  });
 };
+
+const putEditAPI = async (req, res) => {
+  let { email, name, city } = req.body;
+  let id = req.body.id;
+
+  let user = await User.updateOne(
+    { _id: id },
+    {
+      name: name,
+      email: email,
+      city: city,
+    }
+  );
+  return res.status(200).json({
+    EC: 0,
+    data: user,
+  });
+};
+
+const DeleteAPI = async (req, res) => {
+  let { id, email, name, city } = req.body;
+
+  console.log("check id", id);
+
+  let user = await User.deleteOne({ _id: id });
+  return res.status(200).json({
+    EC: 0,
+    data: user,
+  });
+};
+
 module.exports = {
   getUsersAPI,
   postCreateUserAPI,
+  putEditAPI,
+  DeleteAPI,
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getUsersAPI, postCreateUserAPI } = require("../controllers/apiController");
+const { getUsersAPI, postCreateUserAPI, putEditAPI, DeleteAPI } = require("../controllers/apiController");
 
 const routerAPI = express.Router();
 
 
 routerAPI.get("/users",getUsersAPI);
 routerAPI.post("/users",postCreateUserAPI);
+routerAPI.put("/users",putEditAPI);
+routerAPI.delete("/users",DeleteAPI);
 
 module.exports = routerAPI;
